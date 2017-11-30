d7841b79b37ad4e82817fcecb7962cf839a5df33
Kareem-Emad <kareememad400@gmail.com>
2017-11-30 14:18:33 +0200

    Deleting libs

--- _GameEngine/GameEngine/Build/launch-main_window-RelWithDebInfo.cmd
+++
@@ -1,9 +0,0 @@
-cd D:/Github projects/CG-Project/_GameEngine/GameEngine/main_window/
-set PATH=;%PATH%
-
-if [%1]==[--debugger] (
-	SHIFT
-)
-
-"D:\Github projects\CG-Project\_GameEngine\GameEngine\Build\RelWithDebInfo\main_window.exe"  
-pause
